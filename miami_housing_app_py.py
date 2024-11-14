a402662ee1fab7ff74281d511c0d31ff1701586c
Sanjeev N <SANJEEV0906198@GMAIL.COM>
2024-11-14 18:46:20 +0100

    Update miami_housing_app_py.py

--- miami_housing_app_py.py
+++ miami_housing_app_py.py
@@ -53,7 +53,6 @@ def main():
     })
     if st.button("Predict"):
         with st.spinner('Calculating...'):  # Display a spinner while predicting
-            time.sleep(1)  # Simulate some processing time
             prediction = model.predict(input_data)[0]
             st.success(f"Predicted Price: ${prediction:,.2f}")
             st.balloons()  # Show balloons after prediction
